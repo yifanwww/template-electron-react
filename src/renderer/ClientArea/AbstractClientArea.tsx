3490c75f011443c762c9434f6ba48887ca3f9745
YF1999 <1586939317@qq.com>
2020-09-22 15:14:00 +0800

    Feat: Improve Window Control, update AbstractClientArea

--- src/renderer/ClientArea/AbstractClientArea.tsx
+++ src/renderer/ClientArea/AbstractClientArea.tsx
@@ -1,5 +1,11 @@
 import { Component } from 'react';
 import { IpcRendererEvent } from 'electron';
+import { IpcRenderer } from '@Electron';
+import { PartWindowChannels } from '@Utils/Window';
+
+type IpcEvent<Args = undefined> = Args extends undefined
+    ? (event: IpcRendererEvent) => void
+    : (event: IpcRendererEvent, args: Args) => void;
 
 export interface Size {
     width: number;
@@ -7,7 +13,15 @@ export interface Size {
 }
 
 export abstract class AbstractClientArea<P, S> extends Component<P, S> {
+    protected readonly _partWindowChannels: PartWindowChannels;
+
     // --------------------------------------------------------------------------------------- React
+
+    public constructor(props: Readonly<P>, partWindowChannels: PartWindowChannels) {
+        super(props);
+
+        this._partWindowChannels = partWindowChannels;
+    }
 
     public componentDidMount(): void {
         this._AddIpcListeners();
@@ -21,18 +35,35 @@ export abstract class AbstractClientArea<P, S> extends Component<P, S> {
 
     // ------------------------------------------------------------------------------- Ipc Listeners
 
-    protected abstract _AddIpcOnceListeners(): void;
-    protected abstract _AddIpcListeners(): void;
-    protected abstract _RemoveIpcListeners(): void;
+    protected _AddIpcOnceListeners(): void {
+        IpcRenderer.Once(
+            this._partWindowChannels.ClientAreaInitialized,
+            this._Wrapper_ClientAreaInitialized
+        );
+    }
 
-    protected abstract _SendIpcMessage(): void;
+    protected _AddIpcListeners(): void {
+        IpcRenderer.On(this._partWindowChannels.WindowResized, this._Wrapper_WindowResized);
+    }
+
+    protected _RemoveIpcListeners(): void {
+        IpcRenderer.RemoveListener(
+            this._partWindowChannels.WindowResized,
+            this._Wrapper_WindowResized
+        );
+    }
+
+    protected _SendIpcMessage(): void {
+        IpcRenderer.Send(this._partWindowChannels.ClientAreaInitialized);
+    }
 
     // ---------------------------------------------------------------------------------- Ipc Events
 
-    protected abstract _OnceClientAreaInitialized(
-        event: IpcRendererEvent,
-        clientAreaSize: Size
-    ): void;
+    private _Wrapper_ClientAreaInitialized: IpcEvent<Size> = (event, clientAreaSize) =>
+        this._OnceClientAreaInitialized(event, clientAreaSize);
+    protected _OnceClientAreaInitialized(event: IpcRendererEvent, clientAreaSize: Size): void {}
 
-    protected abstract _OnWindowResized(event: IpcRendererEvent, clientAreaSize: Size): void;
+    private _Wrapper_WindowResized: IpcEvent<Size> = (event, clientAreaSize) =>
+        this._OnWindowResized(event, clientAreaSize);
+    protected _OnWindowResized(event: IpcRendererEvent, clientAreaSize: Size): void {}
 }
--- src/renderer/ReactApp/index.tsx
+++ src/renderer/ReactApp/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 
-import { IpcRenderer } from '@Electron';
 import { WindowChannels } from '@Utils/Window';
 import { AbstractClientArea, Size } from '@ClientArea';
 
@@ -16,7 +15,7 @@ class ClientArea extends AbstractClientArea<{}, ClientAreaState> {
     // --------------------------------------------------------------------------------------- React
 
     public constructor(props: Readonly<{}>) {
-        super(props);
+        super(props, WindowChannels.MainWindow);
 
         this.state = {
             clientAreaSize: { width: 1280, height: 720 },
@@ -31,38 +30,17 @@ class ClientArea extends AbstractClientArea<{}, ClientAreaState> {
         );
     }
 
-    // ------------------------------------------------------------------------------- Ipc Listeners
-
-    protected _AddIpcOnceListeners(): void {
-        IpcRenderer.Once(
-            WindowChannels.MainWindow.ClientAreaInitialized,
-            this._OnceClientAreaInitialized
-        );
-    }
-
-    protected _AddIpcListeners(): void {
-        IpcRenderer.On(WindowChannels.MainWindow.WindowResized, this._OnWindowResized);
-    }
-
-    protected _RemoveIpcListeners(): void {
-        IpcRenderer.RemoveListener(WindowChannels.MainWindow.WindowResized, this._OnWindowResized);
-    }
-
-    protected _SendIpcMessage(): void {
-        IpcRenderer.Send(WindowChannels.MainWindow.ClientAreaInitialized);
-    }
-
     // ---------------------------------------------------------------------------------- Ipc Events
 
-    protected _OnceClientAreaInitialized = (event: any, clientAreaSize: Size): void => {
+    protected _OnceClientAreaInitialized(event: any, clientAreaSize: Size): void {
         console.log(clientAreaSize);
         this.setState({ clientAreaSize: clientAreaSize });
-    };
+    }
 
-    protected _OnWindowResized = (event: any, clientAreaSize: Size): void => {
+    protected _OnWindowResized(event: any, clientAreaSize: Size): void {
         console.log(clientAreaSize);
         this.setState({ clientAreaSize: clientAreaSize });
-    };
+    }
 }
 
 export { ClientArea as ReactAppClientArea };
