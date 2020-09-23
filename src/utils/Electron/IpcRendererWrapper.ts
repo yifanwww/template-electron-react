644300723f745ab74db664e090cd29f304e1229b
YF1999 <1586939317@qq.com>
2020-09-23 18:37:48 +0800

    Refactor: No more args in IpcRenderer's methods

--- src/utils/Electron/IpcRendererWrapper.ts
+++ src/utils/Electron/IpcRendererWrapper.ts
@@ -1,20 +1,27 @@
+import { IpcRendererEvent } from 'electron';
+
 export class IpcRendererWrapper {
     private ipc?: Electron.IpcRenderer;
 
-    public constructor(ipc?: Electron.IpcRenderer) {
-        this.ipc = ipc;
+    public constructor() {
+        try {
+            let { ipcRenderer } = window.require('electron');
+            this.ipc = ipcRenderer;
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
-    public async Invoke(channel: string, ...args: any[]): Promise<any> {
-        this.ipc?.invoke(channel, ...args);
+    public async Invoke(channel: string, args?: any): Promise<any> {
+        return this.ipc?.invoke(channel, args);
     }
 
-    public On(channel: string, listener: (...args: any[]) => void): this {
+    public On(channel: string, listener: (event: IpcRendererEvent, args?: any) => void): this {
         this.ipc?.on(channel, listener);
         return this;
     }
 
-    public Once(channel: string, listener: (...args: any[]) => void): this {
+    public Once(channel: string, listener: (event: IpcRendererEvent, args?: any) => void): this {
         this.ipc?.once(channel, listener);
         return this;
     }
@@ -28,24 +35,27 @@ export class IpcRendererWrapper {
         return this;
     }
 
-    public RemoveListener(channel: string, listener: (...args: any[]) => void): this {
+    public RemoveListener(
+        channel: string,
+        listener: (event: IpcRendererEvent, args?: any) => void
+    ): this {
         this.ipc?.removeListener(channel, listener);
         return this;
     }
 
-    public Send(channel: string, ...args: any[]): void {
-        this.ipc?.send(channel, ...args);
+    public Send(channel: string, args?: any): void {
+        this.ipc?.send(channel, args);
     }
 
-    public SendSync(channel: string, ...args: any[]): any {
-        return this.ipc?.sendSync(channel, ...args);
+    public SendSync(channel: string, args?: any): any {
+        return this.ipc?.sendSync(channel, args);
     }
 
-    public SendTo(webContentsId: number, channel: string, ...args: any[]): void {
-        this.ipc?.sendTo(webContentsId, channel, ...args);
+    public SendTo(webContentsId: number, channel: string, args?: any): void {
+        this.ipc?.sendTo(webContentsId, channel, args);
     }
 
-    public SendToHost(channel: string, ...args: any[]): void {
-        this.ipc?.sendToHost(channel, ...args);
+    public SendToHost(channel: string, args?: any): void {
+        this.ipc?.sendToHost(channel, args);
     }
 }
--- src/utils/Electron/index.ts
+++ src/utils/Electron/index.ts
@@ -1,12 +1,3 @@
 import { IpcRendererWrapper } from './IpcRendererWrapper';
 
-let _ipc: Electron.IpcRenderer | undefined;
-
-try {
-    let { ipcRenderer } = window.require('electron');
-    _ipc = ipcRenderer;
-} catch (error) {
-    console.error(error.message);
-}
-
-export const IpcRenderer = new IpcRendererWrapper(_ipc);
+export const IpcRenderer = new IpcRendererWrapper();
