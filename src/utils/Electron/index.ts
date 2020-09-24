f395c70fd638c1c31e4a30cb377504f66736ef6c
YF1999 <1586939317@qq.com>
2020-09-24 19:25:24 +0800

    Feat: Export BaseIpcMainClass

--- src/utils/Electron/index.ts
+++ src/utils/Electron/index.ts
@@ -4,6 +4,6 @@ export type { IpcMainFunc, IpcMainInvokeFunc } from './IpcMainWrapper';
 export { IpcRendererWrapper } from './IpcRendererWrapper';
 export type { IpcRendererFunc } from './IpcRendererWrapper';
 
-export { BaseIpcMain } from './BaseIpcMain';
+export { BaseIpcMainClass, BaseIpcMain } from './BaseIpcMain';
 export { BaseIpcRendererClass, BaseIpcRenderer } from './BaseIpcRenderer';
 export type { ClientAreaSize } from './BaseIpc.types';
