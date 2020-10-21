43e53469b04b3df32517c1c248bb78bdf5a3c36a
YF1999 <1586939317@qq.com>
2020-10-22 05:54:29 +0800

    Chore: Update eslint rc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-    extends: ['plugin:react/recommended', 'eslint:recommended', 'airbnb-typescript/base'],
+    extends: ['airbnb-typescript/base'],
     plugins: [
         '@typescript-eslint',
         'flowtype',
@@ -14,37 +14,83 @@ module.exports = {
     parserOptions: {
         project: './tsconfig.json',
     },
+    env: {
+        browser: true,
+        es2017: true,
+        jest: true,
+        node: true,
+    },
     rules: {
         '@typescript-eslint/comma-dangle': 'off',
-        '@typescript-eslint/indent': ['warn', 4],
+
+        // Failed for some generic type definitions
+        '@typescript-eslint/indent': 'off',
+
         '@typescript-eslint/lines-between-class-members': 'off',
-        '@typescript-eslint/no-loop-func': 'off',
-        '@typescript-eslint/no-shadow': 'off',
-        '@typescript-eslint/no-use-before-define': 'off',
+
+        '@typescript-eslint/naming-convention': [
+            'error',
+            // The accessor names shouldn't be the same as some fields
+            { selector: 'accessor', format: ['PascalCase'] },
+            { selector: 'class', format: ['PascalCase'] },
+            { selector: 'enum', format: ['PascalCase'] },
+            { selector: 'enumMember', format: ['camelCase'] },
+            // react function components
+            { selector: 'function', format: ['camelCase', 'PascalCase'] },
+            { selector: 'interface', format: ['PascalCase'] },
+            { selector: 'method', format: ['camelCase'] },
+            { selector: 'parameter', format: ['camelCase'] },
+            { selector: 'property', format: ['camelCase'] },
+            { selector: 'typeAlias', format: ['PascalCase'] },
+            { selector: 'typeParameter', format: ['PascalCase'] },
+            // react components from react-redux.connect, constant variables
+            { selector: 'variable', format: ['camelCase', 'PascalCase', 'UPPER_CASE'] },
+        ],
+
+        '@typescript-eslint/no-shadow': ['error', { allow: ['props', 'resolve', 'theme'] }],
+
+        // Disabled for prettier
         'arrow-body-style': 'off',
+
         'class-methods-use-this': 'off',
-        curly: 'off',
-        'guard-for-in': 'off', // XXX
-        'function-paren-newline': 'off',
+
+        // XXX
+        'guard-for-in': 'off',
+
         'implicit-arrow-linebreak': 'off',
+
+        // Seems not working for tsx extension
         'import/extensions': 'off',
+        // 'import/extensions': [
+        //     'error',
+        //     'always',
+        //     { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
+        // ],
+
+        // Disable for redux
         'import/no-cycle': 'off',
+
+        // Disabled for import-statement of 'electron'
+        'import/no-extraneous-dependencies': 'off',
+
         'import/prefer-default-export': 'off',
-        'max-classes-per-file': ['warn', 2],
-        'max-len': ['warn', { code: 100 }],
+
+        'max-len': ['error', { code: 100 }],
+
         'no-continue': 'off',
-        'no-console': 'off', // XXX
+
+        'no-console': ['error', { allow: ['warn', 'error'] }],
+
         'no-else-return': 'off',
+
         'no-param-reassign': 'off',
+
         'no-restricted-syntax': 'off',
-        'no-undef': 'off',
-        'nonblock-statement-body-position': 'off',
+
+        // Disabled for import-statement
         'object-curly-newline': 'off',
-        'object-shorthand': 'off',
+
+        // Disabled for prettier
         'operator-linebreak': 'off',
-        'react/prop-types': 'off',
-    },
-    env: {
-        'jest/globals': true,
     },
 };
