a55e676e6a5ae41df740cbb1919cb12d9447dacf
YF1999 <1586939317@qq.com>
2020-12-06 04:18:26 +0800

    chore: Update eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,82 +21,61 @@ module.exports = {
         'react-hooks',
     ],
     rules: {
+        'arrow-body-style': 'off', // Disabled for prettier
+        'class-methods-use-this': 'off',
+        'consistent-return': 'off',
+        'guard-for-in': 'off',
+        'implicit-arrow-linebreak': 'off',
+        'max-len': ['error', { code: 100 }],
+        'no-continue': 'off',
+        'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
+        'no-else-return': 'off',
+        'no-param-reassign': 'off',
+        'no-plusplus': 'off',
+        'no-restricted-syntax': 'off',
+        'no-undef': 'off', // Disabled for JSX.Element
+        'no-underscore-dangle': 'off', // Disabled for leading-underscore options of '@typescript-eslint/naming-convention'
+        'object-curly-newline': 'off', // Disabled for prettier
+        'operator-linebreak': 'off', // Disabled for prettier
+
         '@typescript-eslint/comma-dangle': 'off',
-
-        // Failed for some generic type definitions
-        '@typescript-eslint/indent': 'off',
-
+        '@typescript-eslint/indent': 'off', // Failed for some generic type definitions
         '@typescript-eslint/lines-between-class-members': 'off',
-
         '@typescript-eslint/naming-convention': [
             'error',
-            // The accessor names shouldn't be the same as some fields
-            { selector: 'accessor', format: ['PascalCase'] },
+            { selector: 'accessor', format: ['PascalCase'] }, // The accessor names shouldn't be the same as properties
             { selector: 'class', format: ['PascalCase'] },
             { selector: 'enum', format: ['PascalCase'] },
             { selector: 'enumMember', format: ['camelCase'] },
-            // react function components
-            { selector: 'function', format: ['camelCase', 'PascalCase'] },
+            {
+                // 'PascalCase' for react function components
+                selector: 'function',
+                format: ['camelCase', 'PascalCase'],
+                leadingUnderscore: 'allow',
+            },
             { selector: 'interface', format: ['PascalCase'] },
-            { selector: 'method', format: ['camelCase'] },
-            { selector: 'parameter', format: ['camelCase'] },
-            { selector: 'property', format: ['camelCase'] },
+            { selector: 'method', format: ['camelCase'], leadingUnderscore: 'allow' },
+            { selector: 'parameter', format: ['camelCase'], leadingUnderscore: 'allow' },
+            { selector: 'property', format: ['camelCase'], leadingUnderscore: 'allow' },
             { selector: 'typeAlias', format: ['PascalCase'] },
             { selector: 'typeParameter', format: ['PascalCase'] },
-            // react components from react-redux.connect, constant variables
-            { selector: 'variable', format: ['camelCase', 'PascalCase', 'UPPER_CASE'] },
+            {
+                // HACK: 'PascalCase' for react components from react-redux.connect, 'UPPER_CASE' for constant variables
+                selector: 'variable',
+                format: ['camelCase', 'PascalCase', 'UPPER_CASE'],
+                leadingUnderscore: 'allow',
+            },
         ],
-
         '@typescript-eslint/no-shadow': ['error', { allow: ['props', 'resolve', 'theme'] }],
 
-        // Disabled for prettier
-        'arrow-body-style': 'off',
-
-        'class-methods-use-this': 'off',
-
-        'consistent-return': 'off',
-
-        'guard-for-in': 'off',
-
-        'implicit-arrow-linebreak': 'off',
-
-        // Seems not working for tsx extension
-        'import/extensions': 'off',
+        'import/extensions': 'off', // Seems not working for tsx extension
         // 'import/extensions': [
         //     'error',
         //     'always',
         //     { js: 'never', jsx: 'never', ts: 'never', tsx: 'never' },
         // ],
-
-        // Disable for redux
-        'import/no-cycle': 'off',
-
-        // Disabled for import-statement of 'electron'
-        'import/no-extraneous-dependencies': 'off',
-
+        'import/no-cycle': 'off', // HACK: Disabled for redux
+        'import/no-extraneous-dependencies': 'off', // Disabled for import-statement of 'electron'
         'import/prefer-default-export': 'off',
-
-        'max-len': ['error', { code: 100 }],
-
-        'no-continue': 'off',
-
-        'no-console': ['error', { allow: ['info', 'warn', 'error'] }],
-
-        'no-else-return': 'off',
-
-        'no-param-reassign': 'off',
-
-        'no-plusplus': 'off',
-
-        'no-restricted-syntax': 'off',
-
-        // Disabled for JSX.Element
-        'no-undef': 'off',
-
-        // Disabled for import-statement
-        'object-curly-newline': 'off',
-
-        // Disabled for prettier
-        'operator-linebreak': 'off',
     },
 };
