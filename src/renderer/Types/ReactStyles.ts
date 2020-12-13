894af972b232400338b57e1b40ee0686eb46df29
YF1999 <1586939317@qq.com>
2020-12-13 10:35:18 +0800

    feat(renderer): Update type definitions for React styles

--- src/renderer/Types/ReactStyles.ts
+++ src/renderer/Types/ReactStyles.ts
@@ -1,3 +1,21 @@
 import { CSSProperties } from 'react';
 
-export type ReactStyleFunc = () => CSSProperties;
+export type ReactStyleFunc<
+    A = undefined,
+    A2 = undefined,
+    A3 = undefined,
+    A4 = undefined,
+    A5 = undefined,
+    A6 = undefined,
+    A7 = undefined,
+    A8 = undefined
+> = (
+    arg?: A,
+    arg2?: A2,
+    arg3?: A3,
+    arg4?: A4,
+    arg5?: A5,
+    arg6?: A6,
+    arg7?: A7,
+    arg8?: A8,
+) => CSSProperties;
