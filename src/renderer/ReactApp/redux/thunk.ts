4b92ae9c3ba5b9dc0950f89a9e1493bdd7ede7d2
YF1999 <1586939317@qq.com>
2020-12-17 18:18:11 +0800

    feat(redux): Update thunks

--- src/renderer/ReactApp/redux/thunk.ts
+++ src/renderer/ReactApp/redux/thunk.ts
@@ -7,6 +7,6 @@ import { StoreState } from './store';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 type AppThunk<ReturnType = void> = ThunkAction<ReturnType, StoreState, unknown, Action<string>>;
 
-export const thunk = {};
+export const thunks = {};
 
-export type MapThunksToProps = IMapThunksToProps<typeof thunk>;
+export type MapThunksToProps = IMapThunksToProps<typeof thunks>;
