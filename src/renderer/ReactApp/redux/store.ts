bb8392827ac97af9d25d3e99c9eaf55e34d7e2d0
YF1999 <1586939317@qq.com>
2020-12-18 09:54:02 +0800

    feat(ReactApp): Add sdispatch abbr of store.dispatch

--- src/renderer/ReactApp/redux/store.ts
+++ src/renderer/ReactApp/redux/store.ts
@@ -3,5 +3,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { reducer } from './slice';
 
 export const store = configureStore({ reducer });
+export const sdispatch = store.dispatch;
 
 export type StoreState = ReturnType<typeof store.getState>;
