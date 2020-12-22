77fe85457847c0f2b5ae88b03ce34c6b0f8e7fb2
YF1999 <1586939317@qq.com>
2020-12-22 16:14:40 +0800

    scripts: Update utils.mjs

--- scripts/utils.mjs
+++ scripts/utils.mjs
@@ -4,15 +4,19 @@ import _path from 'path';
 import _url from 'url';
 
 const __dirname = _path.dirname(_url.fileURLToPath(import.meta.url));
-const workingDir = _path.join(__dirname, '../working');
+export const workingDir = _path.join(__dirname, '../working');
+
+export async function createFolder(dir) {
+    try {
+        await _fs.promises.mkdir(dir);
+        console.info(`Create directory "${dir}".`);
+    } catch (error) {
+        console.info(`Directory "${dir}" exists.`);
+    }
+}
 
 export async function createWorkingDir() {
-    try {
-        await _fs.promises.mkdir(workingDir);
-        console.info(`Created folder "${workingDir}".`);
-    } catch (error) {}
-
-    return;
+    return createFolder(workingDir);
 }
 
 export async function executeElectron(command) {
