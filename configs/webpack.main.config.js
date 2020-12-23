172a7e2b315849504178c4c1d071ee1d26f6a58c
YF1999 <1586939317@qq.com>
2020-12-23 13:09:13 +0800

    chore: Update webpack configs

--- configs/webpack.main.config.js
+++ configs/webpack.main.config.js
@@ -3,31 +3,37 @@ const smp = new SpeedMeasurePlugin({ outputFormat: 'humanVerbose' });
 
 const Base = require('./webpack.base.config');
 
-module.exports = smp.wrap({
-    target: 'electron-main',
-    entry: Base.Paths.AppIndexJsMain,
-    output: {
-        filename: 'electron.js',
-        path: Base.Paths.AppBuild,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: 'ts-loader',
-                include: [Base.Paths.AppSrcMain, Base.Paths.AppSrcShared],
-                options: {
-                    configFile: Base.Paths.AppTsConfigMain,
+module.exports = (env, argv) => {
+    const isEnvProduction = argv.mode === 'production';
+
+    const webpack = {
+        target: 'electron-main',
+        entry: Base.Paths.AppIndexJsMain,
+        output: {
+            filename: 'electron.js',
+            path: Base.Paths.AppBuild,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    loader: 'ts-loader',
+                    include: [Base.Paths.AppSrcMain, Base.Paths.AppSrcShared],
+                    options: {
+                        configFile: Base.Paths.AppTsConfigMain,
+                    },
                 },
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts'],
-        alias: Base.Alias,
-    },
-    node: {
-        __dirname: false,
-        __filename: false,
-    },
-});
+            ],
+        },
+        resolve: {
+            extensions: ['.js', 'mjs', '.ts'],
+            alias: Base.Alias,
+        },
+        node: {
+            __dirname: false,
+            __filename: false,
+        },
+    };
+
+    return isEnvProduction ? smp.wrap(webpack) : webpack;
+};
