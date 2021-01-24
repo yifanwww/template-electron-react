1b4cd6db99e120b18ded32a2ed33cc4b2c8cfb9c
YF1999 <1586939317@qq.com>
2021-01-25 00:35:52 +0800

    feat: Use 'electron-devtools-installer' to install electorn extensions in development environment

--- src/main/app.ts
+++ src/main/app.ts
@@ -6,7 +6,25 @@ import { createWindow } from './Window';
 
 // This method will be called when Electron has finished initialization and is ready to create
 // browser windows.
-app.on('ready', () => createWindow(WindowType.main));
+app.on('ready', () => {
+    if (process.env.NODE_ENV === 'production') {
+        console.info('No extensions will be installed.');
+    } else {
+        /* eslint-disable global-require */
+        const {
+            default: installExtensions,
+            REDUX_DEVTOOLS,
+            REACT_DEVELOPER_TOOLS,
+        } = require('electron-devtools-installer');
+        /* eslint-enable global-require */
+
+        installExtensions([REDUX_DEVTOOLS, REACT_DEVELOPER_TOOLS])
+            .then((name: string) => console.info(`Added extensions: ${name}`))
+            .catch((err: any) => console.error('An error occurred:', err));
+    }
+
+    createWindow(WindowType.main);
+});
 
 app.on('window-all-closed', () => {
     // On macOS, most applications and their menu bars will key activated unless users use
