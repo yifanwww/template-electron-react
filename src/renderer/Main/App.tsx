6de361456ecfea8dd5c8093dae466b38ffb9822d
YF1999 <1586939317@qq.com>
2021-02-18 16:54:07 +0800

    feat: Use mergeStyleSets to apply css styles instead of using css file

--- src/renderer/Main/App.tsx
+++ src/renderer/Main/App.tsx
@@ -1,17 +1,68 @@
-import { ReactElement } from 'react';
+import { ReactElement, useMemo } from 'react';
+import { IStyle, keyframes, mergeStyleSets } from '@fluentui/react';
+
+import { IStrictStyle } from '#RUtils/Fluentui';
 
 import logo from './logo.svg';
-import './App.css';
+
+interface TStyleSet {
+    app: IStrictStyle;
+    appHeader: IStrictStyle;
+    appLink: IStrictStyle;
+    appLogo: IStyle;
+}
 
 export function App(): ReactElement {
+    const classNames = useMemo(() => {
+        const appLogoKeyframes = keyframes({
+            from: { transform: 'rotate(0deg)' },
+            to: { transform: 'rotate(360deg)' },
+        });
+
+        return mergeStyleSets<TStyleSet>({
+            app: {
+                displayName: 'App',
+
+                textAlign: 'center',
+            },
+            appHeader: {
+                displayName: 'App-header',
+
+                alignItems: 'center',
+                backgroundColor: '#282c34',
+                color: 'white',
+                display: 'flex',
+                flexDirection: 'column',
+                fontSize: 'calc(10px + 2vmin)',
+                justifyContent: 'center',
+                minHeight: '100vh',
+            },
+            appLink: {
+                displayName: 'App-link',
+
+                color: '#61dafb',
+            },
+            appLogo: {
+                displayName: 'App-logo',
+
+                height: '40vmin',
+                pointerEvents: 'none',
+
+                '@media (prefers-reduced-motion: no-preference)': {
+                    animation: `${appLogoKeyframes} infinite 20s linear`,
+                },
+            },
+        });
+    }, []);
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
+        <div className={classNames.app}>
+            <header className={classNames.appHeader}>
+                <img src={logo} className={classNames.appLogo} alt="logo" />
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                <a className={classNames.appLink} href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
                     Learn React
                 </a>
             </header>
