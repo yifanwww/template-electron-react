4865bb33da1502d28d8371a5f4cf803b44c96221
YF1999 <1586939317@qq.com>
2021-04-28 19:21:06 +0800

    refactor(react): Update useCountdown

--- src/renderer/Utils/React/Hooks/useCountdown.ts
+++ src/renderer/Utils/React/Hooks/useCountdown.ts
@@ -2,25 +2,25 @@ import { useCallback, useEffect, useState } from 'react';
 
 type SetCountdown = (countdown: number) => void;
 
-export function useCountdown(countdown: number): [number, SetCountdown] {
-    const [targetTime, setTargetTime] = useState(Math.round(Date.now() / 1000 + countdown));
-    const [currentTime, setCurrentTime] = useState(Math.round(Date.now() / 1000));
+const getCurrentTime = () => Math.round(Date.now() / 1000);
+const getTargetTime = (countdown: number) => Math.round(Date.now() / 1000 + countdown);
 
-    const setCountdown = useCallback<SetCountdown>(
-        (_countdown) => setTargetTime(Math.round(Date.now() / 1000 + _countdown)),
-        [setTargetTime],
-    );
+export function useCountdown(countdown: number): [number, SetCountdown] {
+    const [targetTime, setTargetTime] = useState(getTargetTime(countdown));
+    const [currentTime, setCurrentTime] = useState(getCurrentTime());
+
+    const setCountdown = useCallback((_countdown: number) => setTargetTime(getTargetTime(_countdown)), [setTargetTime]);
 
     useEffect(() => {
         const interval = setInterval(() => {
-            const ts = Math.round(Date.now() / 1000);
-            if (currentTime !== ts) {
-                setCurrentTime(ts);
+            const _currentTime = getCurrentTime();
+            if (currentTime !== _currentTime) {
+                setCurrentTime(_currentTime);
             }
         });
 
         return () => clearInterval(interval);
     }, [currentTime]);
 
-    return [targetTime - currentTime, setCountdown];
+    return [targetTime - currentTime >= 0 ? targetTime - currentTime : 0, setCountdown];
 }
