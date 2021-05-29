1884b2a71e0a79c902e244d5ee0f8270e3bb12bb
YF1999 <1586939317@qq.com>
2021-05-29 11:03:53 +0800

    feat(shared/typeutils): Change 'DeepReadonly' to support making more structures deep readonly

--- src/shared/TypeUtils.ts
+++ src/shared/TypeUtils.ts
@@ -12,19 +12,34 @@ export type DeepPartial<T> = {
                 : T[P];
 };
 
+export type Primitive = string | number | boolean | bigint | symbol | undefined | null;
+export type Builtin = Primitive | Function | Date | Error | RegExp;
+
 /**
  * TypeScript type to return a deep readonly object (recursively).
  */
 // prettier-ignore
-export type DeepReadonly<T> = {
-    readonly [P in keyof T]: T[P] extends (infer U)[]
-        ? DeepReadonly<U>[]
-        : T[P] extends (...args: any[]) => any
-            ? T[P]
-            : T[P] extends object
-                ? DeepReadonly<T[P]>
-                : T[P];
-};
+export type DeepReadonly<T> = T extends Builtin
+    ? T
+    : T extends Map<infer K, infer V>
+        ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>>
+        : T extends ReadonlyMap<infer K, infer V>
+            ? ReadonlyMap<DeepReadonly<K>, DeepReadonly<V>>
+            : T extends WeakMap<infer K, infer V>
+                ? WeakMap<DeepReadonly<K>, DeepReadonly<V>>
+                : T extends Set<infer U>
+                    ? ReadonlySet<DeepReadonly<U>>
+                    : T extends ReadonlySet<infer U>
+                        ? ReadonlySet<DeepReadonly<U>>
+                        : T extends WeakSet<infer U>
+                            ? WeakSet<DeepReadonly<U>>
+                            : T extends Promise<infer U>
+                                ? Promise<DeepReadonly<U>>
+                                : T extends (infer U)[]
+                                    ? DeepReadonly<U>[]
+                                    : T extends {}
+                                        ? { readonly [K in keyof T]: DeepReadonly<T[K]> }
+                                        : Readonly<T>
 
 /**
  * T must contains all the properties of U.
