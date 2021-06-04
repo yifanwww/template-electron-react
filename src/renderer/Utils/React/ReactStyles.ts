8acc6f2a1ab67e7cbf1c41b6a76d37ea318e4a38
YF1999 <1586939317@qq.com>
2021-06-04 08:33:06 +0800

    chore(deps): Use exact dev dependency 'prettier' at v2.3.0

--- src/renderer/Utils/React/ReactStyles.ts
+++ src/renderer/Utils/React/ReactStyles.ts
@@ -1,6 +1,5 @@
 import { CSSProperties } from 'react';
 
-// prettier-ignore
 export type ReactStyleFunc<
     Arg = undefined,
     Arg2 = undefined,
@@ -9,14 +8,21 @@ export type ReactStyleFunc<
     Arg5 = undefined,
     Arg6 = undefined,
     Arg7 = undefined,
-    Arg8 = undefined
-> = Arg extends undefined ? () => CSSProperties :
-    Arg2 extends undefined ? (arg: Arg) => CSSProperties :
-    Arg3 extends undefined ? (arg: Arg, arg2: Arg2) => CSSProperties :
-    Arg4 extends undefined ? (arg: Arg, arg2: Arg2, arg3: Arg3) => CSSProperties :
-    Arg5 extends undefined ? (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4) => CSSProperties :
-    Arg6 extends undefined ? (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4, arg5: Arg5) => CSSProperties :
-    Arg7 extends undefined ? (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4, arg5: Arg5, arg6: Arg6) => CSSProperties :
-    Arg8 extends undefined
+    Arg8 = undefined,
+> = Arg extends undefined
+    ? () => CSSProperties
+    : Arg2 extends undefined
+    ? (arg: Arg) => CSSProperties
+    : Arg3 extends undefined
+    ? (arg: Arg, arg2: Arg2) => CSSProperties
+    : Arg4 extends undefined
+    ? (arg: Arg, arg2: Arg2, arg3: Arg3) => CSSProperties
+    : Arg5 extends undefined
+    ? (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4) => CSSProperties
+    : Arg6 extends undefined
+    ? (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4, arg5: Arg5) => CSSProperties
+    : Arg7 extends undefined
+    ? (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4, arg5: Arg5, arg6: Arg6) => CSSProperties
+    : Arg8 extends undefined
     ? (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4, arg5: Arg5, arg6: Arg6, arg7: Arg7) => CSSProperties
     : (arg: Arg, arg2: Arg2, arg3: Arg3, arg4: Arg4, arg5: Arg5, arg6: Arg6, arg7: Arg7, arg8: Arg8) => CSSProperties;
