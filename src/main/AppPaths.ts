37fe5152c6228f54b53483860ad42767d88a47b5
YF1999 <1586939317@qq.com>
2021-06-11 17:51:30 +0800

    feat(main): Add getAppPaths

--- src/main/AppPaths.ts
+++ src/main/AppPaths.ts
@@ -1,12 +1,20 @@
 import path from 'path';
 
-const isInASAR = __dirname.includes('.asar');
+function getAppPaths() {
+    const isInASAR = __dirname.includes('.asar');
 
-export const AppPaths = {
-    appPath: isInASAR
+    const appPath = isInASAR
         ? // The path to folder `resources` in installation root.
           path.resolve(__dirname, '../..')
         : // The path to folder `build` in project root.
-          __dirname,
-    srcPath: __dirname,
-};
+          __dirname;
+
+    const srcPath = __dirname;
+
+    return {
+        appPath,
+        srcPath,
+    };
+}
+
+export const AppPaths = getAppPaths();
