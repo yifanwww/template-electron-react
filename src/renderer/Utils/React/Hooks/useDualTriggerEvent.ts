8a5c0cff632315fdfe75d3387f25ad5bfb6201e4
YF1999 <1586939317@qq.com>
2021-07-11 17:20:26 +0800

    feat(utils/react-hooks): Avoid `useDualTriggerEvent` trigger changing while mouse clicking

--- src/renderer/Utils/React/Hooks/useDualTriggerEvent.ts
+++ src/renderer/Utils/React/Hooks/useDualTriggerEvent.ts
@@ -1,4 +1,4 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef } from 'react';
 
 /**
  * Hook to generate a dual trigger event.
@@ -6,23 +6,25 @@ import { useCallback, useState } from 'react';
  *
  * @param dualTriggerEvent A callback function to be triggered as the dual trigger event.
  * @param delayLimit The delay limit in microseconds. A dual trigger event will be triggered if the time difference
- * between the current trigger time and the last trigger time is less than the delay limit.
+ * between the current trigger time and the last trigger time is less than the delay limit. Default is `500`.
  * @returns The trigger.
  */
-export function useDualTriggerEvent(dualTriggerEvent: () => void, delayLimit: number): () => void {
-    const [lastTriggerTime, setTriggerTime] = useState(0);
+export function useDualTriggerEvent(dualTriggerEvent?: () => void, delayLimit: number = 500): () => void {
+    const timeRef = useRef(0);
 
     const _delayLimit = delayLimit >= 0 ? delayLimit : 0;
 
     const trigger = useCallback(() => {
-        const currTriggerTime = Date.now();
-        if (currTriggerTime - lastTriggerTime < _delayLimit) {
-            setTriggerTime(0);
-            dualTriggerEvent();
-        } else {
-            setTriggerTime(Date.now());
+        if (dualTriggerEvent) {
+            const currTime = Date.now();
+            if (currTime - timeRef.current < _delayLimit) {
+                timeRef.current = 0;
+                dualTriggerEvent();
+            } else {
+                timeRef.current = currTime;
+            }
         }
-    }, [_delayLimit, dualTriggerEvent, lastTriggerTime]);
+    }, [_delayLimit, dualTriggerEvent]);
 
     return trigger;
 }
