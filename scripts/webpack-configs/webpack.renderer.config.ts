bc6d16bad844e8a3ae549d7416284c94b6bbd0d2
YF1999 <1586939317@qq.com>
2021-07-13 14:56:20 +0800

    chore(test): Fix test for react

--- scripts/webpack-configs/webpack.renderer.config.ts
+++ scripts/webpack-configs/webpack.renderer.config.ts
@@ -7,44 +7,11 @@
 // - webpack.config.js:
 //   https://github.com/facebook/create-react-app/blob/v4.0.3/packages/react-scripts/config/webpack.config.js
 
-// import fs from 'fs';
+import { Config } from '@jest/types';
 import SpeedMeasurePlugin from 'speed-measure-webpack-plugin';
 import { Configuration } from 'webpack';
 
 import { alias as _alias, paths as _paths } from './webpack.base.config';
-
-// Write configurations as json data into file, for debugging.
-// function WriteConfigs(config, filename) {
-//     const configStr = JSON.stringify(config, '', '  ');
-//     fs.writeFileSync(filename, configStr);
-// }
-
-// Override webpack configurations.
-// The Webpack config to use when compiling your react app for development or production.
-function OverrideWebpackConfigs(webpack: Configuration) {
-    const isEnvProduction = webpack.mode === 'production';
-
-    // 1. Set target to "electron-renderer"
-
-    webpack.target = 'electron-renderer';
-
-    // 2. Set the correct directories where the source code should be compiled.
-
-    // WriteConfigs(webpack.module.rules, 'webpack.json');
-    webpack.module!.rules[1].oneOf![2].include = [_paths.appSrcRenderer, _paths.appSrcShared];
-
-    // 3. Add custom aliases.
-
-    const { alias } = webpack.resolve!;
-    webpack.resolve!.alias = {
-        ...alias,
-        ..._alias,
-    };
-
-    // Finish.
-
-    return isEnvProduction ? new SpeedMeasurePlugin({ outputFormat: 'human' }).wrap(webpack) : webpack;
-}
 
 interface CRAPaths {
     dotent: string;
@@ -69,8 +36,12 @@ interface CRAPaths {
     ownTypeDeclarations: string;
 }
 
-// Override paths configurations.
-function OverridePathsConfigs(paths: CRAPaths) {
+/**
+ * Override paths configurations.
+ *
+ * The paths config to use when compiling your react app for development or production.
+ */
+function overridePathsConfigs(paths: CRAPaths): CRAPaths {
     paths.appHtml = _paths.appHtml;
     paths.appIndexJs = _paths.appIndexTsRenderer;
     paths.appPublic = _paths.appPublic;
@@ -83,7 +54,67 @@ function OverridePathsConfigs(paths: CRAPaths) {
     return paths;
 }
 
+/**
+ * Override webpack configurations.
+ *
+ * The Webpack config to use when compiling your react app for development or production.
+ */
+function overrideWebpackConfigs(webpack: Configuration): Configuration {
+    const isEnvProduction = webpack.mode === 'production';
+
+    // 1. Set target to "electron-renderer"
+
+    webpack.target = 'electron-renderer';
+
+    // 2. Set the correct directories where the source code should be compiled.
+
+    webpack.module!.rules[1].oneOf![2].include = [_paths.appSrcRenderer, _paths.appSrcShared];
+
+    // 3. Add custom aliases.
+
+    const { alias } = webpack.resolve!;
+    webpack.resolve!.alias = {
+        ...alias,
+        ..._alias,
+    };
+
+    // Finish.
+
+    return isEnvProduction ? new SpeedMeasurePlugin({ outputFormat: 'human' }).wrap(webpack) : webpack;
+}
+
+/**
+ * Override jest configurations.
+ *
+ * The Jest config to use when running your jest tests - note that the normal rewires do not work here.
+ */
+function overrideJestConfigs(config: Config.InitialOptions): Config.InitialOptions {
+    // 1. The option `setupFilesAfterEnv` is `<rootDir>/src/setupTests.${setupTestsFileExtension}`, change it.
+
+    config.setupFilesAfterEnv = [_paths.testsSetup];
+
+    // 2. The option `roots` is `['<rootDir>/src']`, change it.
+
+    config.roots = ['<rootDir>/src/renderer', '<rootDir>/src/shared'];
+
+    // 3. The option `collectCoverageFrom` is `['src/**/*.{js,jsx,ts,tsx}', '!src/**/*.d.ts']`, change it.
+
+    config.collectCoverageFrom = ['src/renderer/**/*.{ts,tsx}', 'src/shared/**/*.{ts,tsx}', '!src/**/*.d.ts'];
+
+    // 4. Change the option `testMatch`.
+
+    config.testMatch = [
+        '<rootDir>/src/renderer/**/__tests__/**/*.{ts,tsx}',
+        '<rootDir>/src/renderer/**/*.{spec,test}.{ts,tsx}',
+        '<rootDir>/src/shared/**/__tests__/**/*.{ts,tsx}',
+        '<rootDir>/src/shared/**/*.{spec,test}.{ts,tsx}',
+    ];
+
+    return config;
+}
+
 module.exports = {
-    webpack: OverrideWebpackConfigs,
-    paths: OverridePathsConfigs,
+    webpack: overrideWebpackConfigs,
+    paths: overridePathsConfigs,
+    jest: overrideJestConfigs,
 };
