812ec241ab0e615ad6dd7a074c9dc84fa1b1d5b8
YF1999 <1586939317@qq.com>
2021-07-15 18:56:25 +0800

    feat(utils/react-hooks): Rename `useHover` to `useIsHovered`, add new hook `useIsFocused`

---
+++ src/renderer/Utils/React/Hooks/useIsFocused.ts
@@ -0,0 +1,26 @@
+import { RefObject, useEffect } from 'react';
+
+import { useBoolean } from './useBoolean';
+
+export function useIsFocused<T extends RefObject<HTMLElement>>(ref: T, enabled: boolean = true): boolean {
+    const [isFocused, { setFalse: focusOut, setTrue: focusIn }] = useBoolean(false);
+
+    useEffect(() => {
+        if (enabled && ref.current) {
+            ref.current.addEventListener('focusin', focusIn);
+            ref.current.addEventListener('focusout', focusOut);
+        }
+
+        // fixes react-hooks/exhaustive-deps warning about stale ref elements
+        const { current } = ref;
+
+        return () => {
+            if (enabled && current) {
+                current.removeEventListener('focusin', focusIn);
+                current.removeEventListener('focusout', focusOut);
+            }
+        };
+    }, [enabled, focusIn, focusOut, ref]);
+
+    return isFocused;
+}
