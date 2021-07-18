c1ff72ef96a05f4db6a17c88164600110c62a509
YF1999 <1586939317@qq.com>
2021-07-18 23:11:46 +0800

    feat(utils/style): Add percent2string, add tests for utils/style (#7)
    * feat(utils/style): Add `percent2string`
    
    * test(utils/style): Add tests for utils/style

--- src/renderer/Utils/Style/Num2String.ts
+++ src/renderer/Utils/Style/Num2String.ts
@@ -1,2 +1,5 @@
-export const num2pt = (num: number) => `${num}pt`;
-export const num2px = (num: number) => `${num}px`;
+export type Num2String = (num: number) => string;
+
+export const num2pt: Num2String = (num) => `${num}pt`;
+export const num2px: Num2String = (num) => `${num}px`;
+export const percent2string: Num2String = (num) => `${num}%`;
