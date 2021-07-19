4b393f6909ab5ffc2b3b8763786532f3ef4a465e
YF1999 <1586939317@qq.com>
2021-07-19 10:42:56 +0800

    test(utils/react-hooks): test `useConst` and `useConstFn`

---
+++ src/renderer/Utils/React/Hooks/useConst.test.tsx
@@ -0,0 +1,33 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import { validateHookValueNotChanged } from './testUtils';
+import { useConst } from './useConst';
+
+describe('Test react hook `useConst`', () => {
+    validateHookValueNotChanged('returns the same value with value initializer', () => [useConst(Math.random())]);
+
+    validateHookValueNotChanged('returns the same value with function initializer', () => [useConst(Math.random)]);
+
+    test('calls the function initializer only once', () => {
+        const initializer = jest.fn(() => Math.random());
+        const { rerender, result } = renderHook(() => useConst(initializer));
+
+        const firstValue = result.current;
+        // Re-render the component
+        rerender();
+        // Text should be the same
+        expect(result.current).toBe(firstValue);
+        // Function shouldn't have been called again
+        expect(initializer).toHaveBeenCalledTimes(1);
+    });
+
+    test('works with a function initializer which returns undefined', () => {
+        const initializer = jest.fn(() => undefined);
+        const { rerender } = renderHook(() => useConst(initializer));
+
+        // Re-render the component
+        rerender();
+        // Function shouldn't have been called again
+        expect(initializer).toHaveBeenCalledTimes(1);
+    });
+});
