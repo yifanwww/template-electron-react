25e2b16b64cc135a866c36f3a74b9b7d249b8519
YF1999 <1586939317@qq.com>
2021-07-28 20:03:19 +0800

    feat(ui): Display `Introduction` only after prepared

--- src/renderer/MainWindow/Containers/ClientArea.tsx
+++ src/renderer/MainWindow/Containers/ClientArea.tsx
@@ -2,20 +2,17 @@ import { makeStyles } from '@fluentui/react';
 import { memo, useEffect } from 'react';
 
 import { Introduction } from '../Components/Introduction';
-import { useMainDispatchingThunks } from '../Redux';
+import { useMainDispatchingThunks, usePrepared } from '../Redux';
 
 const useStyles = makeStyles({ clientArea: { display: 'grid' } });
 
 export const ClientArea = memo(function ClientArea() {
     const classes = useStyles();
 
+    const prepared = usePrepared();
     const { prepare } = useMainDispatchingThunks();
 
     useEffect(prepare, [prepare]);
 
-    return (
-        <div className={classes.clientArea}>
-            <Introduction />
-        </div>
-    );
+    return <div className={classes.clientArea}>{prepared && <Introduction />}</div>;
 });
