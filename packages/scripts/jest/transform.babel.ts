94c5804d44997c0cd4cabc88bf6548a3a474a307
yifanwww <yifanw1101@gmail.com>
2021-10-01 02:21:01 +0800

    chore(deps): Refresh deps, upgrade `jest` to v27

--- packages/scripts/jest/transform.babel.ts
+++ packages/scripts/jest/transform.babel.ts
@@ -1,6 +1,6 @@
 import babelJest from 'babel-jest';
 
-export = babelJest.createTransformer({
+export = babelJest.createTransformer!({
     presets: [[require.resolve('babel-preset-react-app'), { runtime: 'automatic' }]],
     babelrc: false,
     configFile: false,
