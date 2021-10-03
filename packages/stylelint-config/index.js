d83e8e965c716e6f13cb07e7145ddda28ef7361a
yifanwww <yifanw1101@gmail.com>
2021-10-03 10:56:23 +0800

    chore: Add dev dep `stylelint-config-recess-order`, update stylelint configs

--- packages/stylelint-config/index.js
+++ packages/stylelint-config/index.js
@@ -1,7 +1,9 @@
 module.exports = {
-    extends: 'stylelint-config-sass-guidelines',
+    extends: ['stylelint-config-sass-guidelines', 'stylelint-config-recess-order'],
     rules: {
         indentation: 4,
         'selector-class-pattern': null,
+
+        'order/properties-alphabetical-order': null,
     },
 };
