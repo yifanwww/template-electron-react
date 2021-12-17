19cd4b0024aa7d64cb5185ea8b5588bc125a9522
yifanwww <yifanw1101@gmail.com>
2021-12-18 01:47:35 +0800

    feat(types): Add some new type utilities

--- packages/global-types/lib/utils.type.d.ts
+++ packages/global-types/lib/utils.type.d.ts
@@ -9,6 +9,16 @@ declare global {
     type Integer = number;
 
     type AnyFn = (...args: unknown[]) => unknown;
+
+    /**
+     * Used for generic function to infer function parameters.
+     *
+     * Example:
+     *
+     * ```ts
+     * function fn<T extends UnknownFn>(cb: T): void {}
+     * ```
+     */
     type UnknownFn = (...args: never[]) => unknown;
 
     /* ----- Type Utils ----- */
--- packages/utils-type/src/utils.ts
+++ packages/utils-type/src/utils.ts
@@ -41,3 +41,75 @@ export type ExcludeUnderscorePrefix<T> = T extends `_${infer R}` ? never : T;
  * Construct a type with the properties of T except for those that start with underscore.
  */
 export type OmitUnderscorePrefix<T extends object> = Pick<T, ExcludeUnderscorePrefix<keyof T>>;
+
+/**
+ * Pick the keys of the properties which are nullable.
+ *
+ * Example:
+ *
+ * ```ts
+ * interface T { a: number; b: string | null; c: null | boolean }
+ * type R = PickNullableKeys<T> // <-- "b" | "c"
+ * ```
+ */
+export type PickNullableKeys<T> = {
+    [K in keyof T]-?: null extends T[K] ? K : never;
+}[keyof T];
+
+/**
+ * Pick the keys of the properties which are undefinable.
+ *
+ * Example:
+ *
+ * ```ts
+ * interface T { a: number; b: string | undefined; c?: boolean }
+ * type R = PickUndefinableKeys<T> // <-- "b" | "c"
+ * ```
+ */
+export type PickUndefinableKeys<T> = {
+    [K in keyof T]-?: undefined extends T[K] ? K : never;
+}[keyof T];
+
+/**
+ * Pick the keys of the properties which are non-nullable.
+ *
+ * Example:
+ *
+ * ```ts
+ * interface T { a: number; b: string | null; c?: number; e: number | undefined; f: string }
+ * type R = PickNonNullableKeys<T> // <-- "a" | "f"
+ * ```
+ */
+export type PickNonNullableKeys<T> = {
+    [K in keyof T]-?: null extends T[K] ? never : undefined extends T[K] ? never : K;
+}[keyof T];
+
+/**
+ * From T, pick a set of properties that are nullable.
+ */
+export type PickNullableProps<T> = Pick<T, PickNullableKeys<T>>;
+
+/**
+ * Construct a type with the properties of T except for those are nullable.
+ */
+export type OmitNullableProps<T> = Omit<T, PickNullableKeys<T>>;
+
+/**
+ * From T, pick a set of properties that are undefinable.
+ */
+export type PickUndefinableProps<T> = Pick<T, PickUndefinableKeys<T>>;
+
+/**
+ * Construct a type with the properties of T except for those are undefinable.
+ */
+export type OmitUndefinableProps<T> = Omit<T, PickUndefinableKeys<T>>;
+
+/**
+ * From T, pick a set of properties that are non-nullable.
+ */
+export type PickNonNullableProps<T> = Pick<T, PickNonNullableKeys<T>>;
+
+/**
+ * Construct a type with the properties of T except for those are non-nullable.
+ */
+export type OmitNonNullableProps<T> = Omit<T, PickNonNullableKeys<T>>;
