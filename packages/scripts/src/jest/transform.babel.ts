6c4d15c8a2fd0ece1efcc8e7c72006f439cdd83c
yifanwww <yifanw1101@gmail.com>
2022-02-16 23:23:55 +0800

    chore: Enable Babel Typescript preset option `allowDeclareFields`

--- packages/scripts/src/jest/transform.babel.ts
+++ packages/scripts/src/jest/transform.babel.ts
@@ -1,7 +1,17 @@
 import babelJest from 'babel-jest';
 
 export = babelJest.createTransformer!({
-    presets: [[require.resolve('babel-preset-react-app'), { runtime: 'automatic' }]],
+    presets: [
+        [require.resolve('babel-preset-react-app'), { runtime: 'automatic' }],
+        [
+            '@babel/preset-typescript',
+            {
+                // Can omit this setting when babel is upgrade above v8
+                // https://github.com/babel/babel/issues/10746
+                allowDeclareFields: true,
+            },
+        ],
+    ],
     babelrc: false,
     configFile: false,
 });
