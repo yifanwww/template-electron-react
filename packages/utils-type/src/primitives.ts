f07d7c3c99c040dd7992fc23dbe2490f58a8eb53
yifanwww <yifanw1101@gmail.com>
2023-06-10 22:08:51 +0800

    refactor(utils-type): change jsdocs

--- packages/utils-type/src/primitives.ts
+++ packages/utils-type/src/primitives.ts
@@ -1,9 +1,9 @@
 /**
- * A number type representing the number of milliseconds elapsed since Unix Epoch (00:00:00 UTC on 1 Jan 1970).
+ * The primitive number type representing the number of milliseconds elapsed since Unix Epoch (00:00:00 UTC 1/1/1970).
  */
 export type UnixEpochMS = number & {};
 
 /**
- * A number type representing the number of seconds elapsed since Unix Epoch (00:00:00 UTC on 1 Jan 1970).
+ * The primitive number type representing the number of seconds elapsed since Unix Epoch (00:00:00 UTC 1/1/1970).
  */
 export type UnixEpochS = number & {};
