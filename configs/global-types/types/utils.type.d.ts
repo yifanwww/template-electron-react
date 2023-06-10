1aceb59f48d4d2b7ba7bde576d687853f72824b3
yifanwww <yifanw1101@gmail.com>
2023-06-11 01:45:31 +0800

    chore(deps): remove `ts-essentials`, can use `type-fest` if need

--- configs/global-types/types/utils.type.d.ts
+++ configs/global-types/types/utils.type.d.ts
@@ -1,78 +1,44 @@
-import type TS from 'ts-essentials';
+/* ----- Types ----- */
 
-declare global {
-    /* ----- Types ----- */
+type Integer = number;
 
-    type Primitive = TS.Primitive;
-    type Builtin = TS.Builtin;
+type AnyFn = (...args: unknown[]) => unknown;
 
-    type Integer = number;
+/**
+ * Used for generic function to infer function parameters.
+ *
+ * Example:
+ *
+ * ```ts
+ * function fn<T extends UnknownFn>(cb: T): void {}
+ * ```
+ */
+type UnknownFn = (...args: never[]) => unknown;
 
-    type AnyFn = (...args: unknown[]) => unknown;
+/* ----- Type Utils ----- */
 
-    /**
-     * Used for generic function to infer function parameters.
-     *
-     * Example:
-     *
-     * ```ts
-     * function fn<T extends UnknownFn>(cb: T): void {}
-     * ```
-     */
-    type UnknownFn = (...args: never[]) => unknown;
+type Optional<T> = T | null;
 
-    /* ----- Type Utils ----- */
+type ValueOf<T> = T[keyof T];
 
-    type Optional<T> = T | null;
+type LooseArray<T> = T | readonly T[];
 
-    type ValueOf<T> = T[keyof T];
+/**
+ * Pick the non-nullable type of a certain property of interface `P`.
+ *
+ * Example:
+ *
+ * ```ts
+ * interface Props { a: boolean; b?: string }
+ * ```
+ * The result of `PickProp<Props, 'a'>` is `boolean`,
+ * the result of `PickProp<Props, 'b'>` is not `string | undefined` but `string`.
+ */
+type PickProp<P, K extends keyof P> = NonNullable<P[K]>;
 
-    type LooseArray<T> = T | readonly T[];
+type VoidReturn<T extends UnknownFn> = (...args: Parameters<T>) => void;
 
-    /**
-     * Pick the non-nullable type of a certain property of interface `P`.
-     *
-     * Example:
-     *
-     * ```ts
-     * interface Props { a: boolean; b?: string }
-     * ```
-     * The result of `PickProp<Props, 'a'>` is `boolean`,
-     * the result of `PickProp<Props, 'b'>` is not `string | undefined` but `string`.
-     */
-    type PickProp<P, K extends keyof P> = NonNullable<P[K]>;
-
-    type VoidReturn<T extends UnknownFn> = (...args: Parameters<T>) => void;
-
-    /**
-     * Obtain the static methods of a class.
-     */
-    type ClassStaticMethods<T extends abstract new (...args: unknown[]) => unknown> = Exclude<keyof T, 'prototype'>;
-
-    /**
-     * TypeScript type to return a deep partial object (each property can be undefined, recursively).
-     */
-    type DeepPartial<T> = TS.DeepPartial<T>;
-
-    /**
-     * TypeScript type to return a deep required object (recursively).
-     */
-    type DeepRequired<T> = TS.DeepRequired<T>;
-
-    /**
-     * TypeScript type to return a deep readonly object (recursively).
-     */
-    type DeepReadonly<T> = TS.DeepReadonly<T>;
-
-    /**
-     * TypeScript type to return a writable object (each property is not readonly).
-     */
-    type Writable<T> = TS.Writable<T>;
-
-    /**
-     * TypeScript type to return a deep writable object (each property is not readonly, recursively).
-     */
-    type DeepWritable<T> = TS.DeepWritable<T>;
-
-    type Merge<M, N> = TS.Merge<M, N>;
-}
+/**
+ * Obtain the static methods of a class.
+ */
+type ClassStaticMethods<T extends abstract new (...args: unknown[]) => unknown> = Exclude<keyof T, 'prototype'>;
