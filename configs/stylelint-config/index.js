a00a342c436ccf71530848550e72469a3c834aab
yifanwww <yifanw1101@gmail.com>
2023-06-20 23:20:15 +0800

    chore(stylelint): disable `max-nesting-depth`

--- configs/stylelint-config/index.js
+++ configs/stylelint-config/index.js
@@ -2,6 +2,8 @@ module.exports = {
     extends: ['stylelint-config-sass-guidelines', 'stylelint-config-recess-order'],
     rules: {
         indentation: 4,
+        // https://stylelint.io/user-guide/rules/max-nesting-depth/
+        'max-nesting-depth': null,
         'selector-class-pattern': null,
         'selector-max-id': null,
 
