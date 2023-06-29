a53ed47a9830526dd05994528296ed5f13dce80c
yifanwww <yifanw1101@gmail.com>
2023-06-29 20:32:16 +0800

    feat: don't change arguments when preloading apis

--- packages/app-main/src/preload/app.ts
+++ packages/app-main/src/preload/app.ts
@@ -3,9 +3,7 @@ import type { AppRendererAPI } from '@tecra/app-common';
 import { ipcRenderer } from 'electron';
 
 export const appAPI: AppRendererAPI = {
-    createWindow: (windowType) => ipcRenderer.invoke(AppAPIChannel.CREATE_WINDOW, windowType),
-
-    getAppDetails: () => ipcRenderer.invoke(AppAPIChannel.GET_APP_DETAILS),
-
-    getWindowType: () => ipcRenderer.invoke(AppAPIChannel.GET_WINDOW_TYPE),
+    createWindow: (...args) => ipcRenderer.invoke(AppAPIChannel.CREATE_WINDOW, ...args),
+    getAppDetails: (...args) => ipcRenderer.invoke(AppAPIChannel.GET_APP_DETAILS, ...args),
+    getWindowType: (...args) => ipcRenderer.invoke(AppAPIChannel.GET_WINDOW_TYPE, ...args),
 };
