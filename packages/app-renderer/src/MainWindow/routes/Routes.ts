358ba98550690b78b09cc2dbad0adae1bc41daed
yifanwww <yifanw1101@gmail.com>
2023-07-01 15:33:55 +0800

    refactor: move files

--- packages/app-renderer/src/MainWindow/routes/Routes.ts
+++ packages/app-renderer/src/MainWindow/routes/Routes.ts
@@ -8,7 +8,7 @@ import { RoutePath } from './RoutePath';
 export const routes: RouteConfig[] = createRoutes([
     {
         path: RoutePath.HOME,
-        component: lazy(() => import(/* webpackChunkName: 'mainwindow-home' */ 'src/MainWindow/containers/HomePage')),
+        component: lazy(() => import(/* webpackChunkName: 'mainwindow-home' */ 'src/MainWindow/pages/HomePage')),
         exact: true,
     },
 ]);
