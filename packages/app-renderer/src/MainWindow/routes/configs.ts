a942c3e0ead4158accb5aae9615979dc822d8fa6
yifanwww <yifanw1101@gmail.com>
2023-08-21 22:36:09 +0800

    feat: remove cyclic imports

--- packages/app-renderer/src/MainWindow/routes/configs.ts
+++ packages/app-renderer/src/MainWindow/routes/configs.ts
@@ -3,7 +3,7 @@ import { lazy } from 'react';
 import type { RouteConfig } from 'src/utils/react-router';
 import { createRoutes } from 'src/utils/react-router';
 
-import { RoutePath } from './RoutePath';
+import { RoutePath } from './path';
 
 export const routes: RouteConfig[] = createRoutes([
     {
