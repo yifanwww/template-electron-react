02eeba27892e17e65ed505918581f2abf8b09faa
yifanwww <yifanw1101@gmail.com>
2023-09-01 16:11:36 +0800

    chore: use esbuild to transpile app-main code

--- packages/scripts/src/electron.ts
+++ packages/scripts/src/electron.ts
@@ -2,25 +2,56 @@ import fs from 'node:fs';
 import { createRequire } from 'node:module';
 import semver from 'semver';
 
-const require = createRequire(import.meta.url);
+const _require = createRequire(import.meta.url);
 
 function getElectronMajorVer(): number {
-    const pkg = require.resolve('electron/package.json');
+    const pkg = _require.resolve('electron/package.json');
 
     if (!fs.existsSync(pkg)) {
         throw new Error('cannot resolve electron package.json');
     }
 
-    // eslint-disable-next-line
-    const version = require(pkg).version as string;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const version = _require(pkg).version as string;
     const majorVer = semver.major(version);
     return majorVer;
+}
+
+export function getElectronNodeTarget(): string {
+    const electronVer = getElectronMajorVer();
+
+    const nodeVer: Record<string, string | undefined> = {
+        '26': '18.16',
+        '25': '18.15',
+        '24': '18.14',
+        '23': '18.12',
+        '22': '16.17',
+        '21': '16.16',
+        '20': '16.15',
+        '19': '16.14',
+        '18': '16.13',
+        '17': '16.13',
+        '16': '16.9',
+        '15': '16.5',
+        '14': '14.17',
+        '13': '14.17',
+        '12': '14.16',
+        '11': '12.18',
+    };
+    if (electronVer > 10) {
+        let target = nodeVer[electronVer];
+        if (!target) target = Object.values(nodeVer).reverse()[0];
+        return `node${target}`;
+    }
+    return '';
 }
 
 export function getElectronChromeTarget(): string {
     const electronVer = getElectronMajorVer();
 
     const chromeVer: Record<string, string | undefined> = {
+        '27': '118',
+        '26': '116',
         '25': '114',
         '24': '112',
         '23': '110',
--- packages/scripts/src/webpack/webpack.main.config.ts
+++ packages/scripts/src/webpack/webpack.main.config.ts
@@ -1,14 +1,14 @@
+import { EsbuildPlugin } from 'esbuild-loader';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
 import { createRequire } from 'node:module';
 import path from 'node:path';
 import url from 'node:url';
-import TerserPlugin from 'terser-webpack-plugin';
 import type { Configuration, WebpackPluginInstance } from 'webpack';
 
+import { getElectronNodeTarget } from '../electron.js';
 import { paths } from '../utils/index.js';
 
 import { createEnvironmentHash } from './utils/createEnvironmentHash.js';
-import { getCacheIdentifier } from './utils/getCacheIdentifier.js';
 import { ReloadElectronWebpackPlugin } from './utils/reloadElectronWebpackPlugin.js';
 import { WebpackStatsPrettifyPlugin } from './utils/webpackStatsPrettifyPlugin.js';
 
@@ -86,44 +86,6 @@ const factory: ConfigurationFactory = (env, argv) => {
             level: 'none',
         },
 
-        optimization: {
-            minimize: isEnvProduction,
-            minimizer: [
-                // This is only used in production mode
-                new TerserPlugin({
-                    terserOptions: {
-                        parse: {
-                            // We want terser to parse ecma 8 code. However, we don't want it to apply any minification
-                            // steps that turns valid ecma 5 code into invalid ecma 5 code. This is why the 'compress'
-                            // and 'output' sections only apply transformations that are ecma 5 safe.
-                            // https://github.com/facebook/create-react-app/pull/4234
-                            ecma: 2017,
-                        },
-                        compress: {
-                            ecma: 5,
-                            // Disabled because of an issue with Uglify breaking seemingly valid code:
-                            // https://github.com/facebook/create-react-app/issues/2376
-                            // Pending further investigation:
-                            // https://github.com/mishoo/UglifyJS2/issues/2011
-                            comparisons: false,
-                            // Disabled because of an issue with Terser breaking valid code:
-                            // https://github.com/facebook/create-react-app/issues/5250
-                            // Pending further investigation:
-                            // https://github.com/terser-js/terser/issues/120
-                            inline: 2,
-                        },
-                        output: {
-                            ecma: 5,
-                            comments: false,
-                            // Turned on because emoji and regex is not minified properly using default
-                            // https://github.com/facebook/create-react-app/issues/2488
-                            ascii_only: true,
-                        },
-                    },
-                }),
-            ],
-        },
-
         resolve: {
             extensions: ['.js', 'mjs', '.ts'],
             alias: {
@@ -147,67 +109,11 @@ const factory: ConfigurationFactory = (env, argv) => {
                 },
                 {
                     oneOf: [
-                        // Process application JS with Babel.
-                        // The preset includes JSX, Flow, TypeScript, and some ESnext features.
                         {
-                            test: /\.(js|mjs|ts)$/,
-                            include: appMainPaths.appSrc,
-                            loader: require.resolve('babel-loader'),
+                            test: /\.[cm]?[jt]s$/,
+                            loader: require.resolve('esbuild-loader'),
                             options: {
-                                customize: require.resolve('babel-preset-react-app/webpack-overrides'),
-                                // TODO: Maybe can change presets
-                                presets: [
-                                    [require.resolve('babel-preset-react-app'), { runtime: 'automatic' }],
-                                    [
-                                        '@babel/preset-typescript',
-                                        {
-                                            // Can omit this setting when babel is upgrade above v8
-                                            // https://github.com/babel/babel/issues/10746
-                                            allowDeclareFields: true,
-                                        },
-                                    ],
-                                ],
-                                babelrc: false,
-                                configFile: false,
-                                // Make sure we have a unique cache identifier, erring on the side of caution.
-                                cacheIdentifier: getCacheIdentifier(isEnvProduction ? 'production' : 'development', [
-                                    'babel-plugin-named-asset-import',
-                                    'babel-preset-react-app',
-                                    '@babel/preset-typescript',
-                                ]),
-                                // This is a feature of `babel-loader` for webpack (not Babel itself).
-                                // It enables caching results in ./node_modules/.cache/babel-loader/
-                                // directory for faster rebuilds.
-                                cacheDirectory: true,
-                                // See #6846 for context on why cacheCompression is disabled
-                                cacheCompression: false,
-                                compact: isEnvProduction,
-                            },
-                        },
-                        // Process any JS outside of the app with Babel.
-                        // Unlike the application JS, we only compile the standard ES features.
-                        {
-                            test: /\.(js|mjs)$/,
-                            exclude: /@babel(?:\/|\\{1,2})runtime/,
-                            loader: require.resolve('babel-loader'),
-                            options: {
-                                babelrc: false,
-                                configFile: false,
-                                compact: false,
-                                // TODO: Maybe can change presets
-                                presets: [[require.resolve('babel-preset-react-app/dependencies'), { helpers: true }]],
-                                cacheDirectory: true,
-                                // See #6846 for context on why cacheCompression is disabled
-                                cacheCompression: false,
-                                cacheIdentifier: getCacheIdentifier(isEnvProduction ? 'production' : 'development', [
-                                    'babel-plugin-named-asset-import',
-                                    'babel-preset-react-app',
-                                ]),
-                                // Babel sourcemaps are needed for debugging into node_modules code.
-                                // Without the options below, debuggers like VSCode show incorrect code and set
-                                // breakpoints on the wrong lines.
-                                sourceMaps: true,
-                                inputSourceMap: true,
+                                target: getElectronNodeTarget(),
                             },
                         },
                     ],
@@ -215,7 +121,17 @@ const factory: ConfigurationFactory = (env, argv) => {
             ],
         },
 
+        optimization: {
+            minimize: isEnvProduction,
+            minimizer: [new EsbuildPlugin({ target: getElectronNodeTarget() })],
+        },
+
         plugins: [
+            new EsbuildPlugin({
+                define: {
+                    'process.env.NODE_ENV': JSON.stringify(isEnvDevelopment ? 'development' : 'production'),
+                },
+            }),
             isEnvDevelopment && new ReloadElectronWebpackPlugin(paths.repository, paths.working),
             // TypeScript type checking
             isEnvDevelopment &&
