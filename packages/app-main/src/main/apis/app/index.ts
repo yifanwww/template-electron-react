88a2b71be73ee034e829d863903a8e4795e1dc68
yifanwww <yifanw1101@gmail.com>
2023-09-16 02:09:01 +0800

    feat: add makeHandler helper fn

--- packages/app-main/src/main/apis/app/index.ts
+++ packages/app-main/src/main/apis/app/index.ts
@@ -5,17 +5,13 @@ import { ipcMain } from 'electron';
 import { getAppDetails } from 'src/main/app';
 import { WindowManager } from 'src/main/window';
 
-function registerCreateWindowHandler() {
-    const handler: AppMainAPI['handleCreateWindow'] = (_, type) => WindowManager.INSTANCE.createWindow({ type });
-    ipcMain.handle(AppAPIChannel.CREATE_WINDOW, handler);
-}
-
-function registerGetAppDetailsHandler() {
-    const handler: AppMainAPI['handleGetAppDetails'] = getAppDetails;
-    ipcMain.handle(AppAPIChannel.GET_APP_DETAILS, handler);
-}
+import { makeHandler } from '../utils';
 
 export function registerAppGlobalHandlers() {
-    registerCreateWindowHandler();
-    registerGetAppDetailsHandler();
+    ipcMain.handle(
+        AppAPIChannel.CREATE_WINDOW,
+        makeHandler<AppMainAPI['handleCreateWindow']>((_, type) => WindowManager.INSTANCE.createWindow({ type })),
+    );
+
+    ipcMain.handle(AppAPIChannel.GET_APP_DETAILS, makeHandler<AppMainAPI['handleGetAppDetails']>(getAppDetails));
 }
