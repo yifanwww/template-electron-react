8a345c7cf95e7b07f848a794a44bec17f20d427b
yifanwww <yifanw1101@gmail.com>
2026-08-31 00:54:31 +0800

    chore(renderer): remove web-vitals dependency and reportWebVitals boilerplate

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './app';
-import { reportWebVitals } from './reportWebVitals';
 import { assertIsDefined } from './utils/assert';
 
 import './index.css';
@@ -16,12 +15,6 @@ function main(): void {
       <App />
     </StrictMode>,
   );
-
-  // If you want to start measuring performance in your app, pass a function to log results
-  // (for example: reportWebVitals (console.log)) or send to an analytics endpoint.
-  // Learn more: https://bit.ly/CRA-vitals
-  // eslint-disable-next-line no-console
-  reportWebVitals(console.info);
 }
 
 main();
